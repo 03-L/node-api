631b80c9dcc51830b51bc6d1377207489eb7f963
L <57785413+03-L@users.noreply.github.com>
2023-05-09 01:44:39 -0500

    Add node-postgres

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const express = require("express");
+const managers = require('./routes/managers');
+
 const app = express();
+
+app.use(express.json());
+app.use('/managers', managers);
 
 app.get("/", (req, res) => {
     res.send("Hello.");
 });
 
-
-
-app.listen(3000, () => console.log("App listening on port 3000."));
+app.listen(process.env.PORT || 3000, () => console.log(`App listening on port ${process.env.PORT || 3000}.`));
